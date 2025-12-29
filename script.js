0af31292afc2becbdac5a0fd2771a6f32de1d19a
Niwea81 <circularevo@gmail.com>
2025-12-29 14:42:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const STORAGE_KEY = "controle_teste_mesas";
-
-// torna todas as células editáveis
-document.querySelectorAll("td").forEach((td) => {
-  td.contentEditable = true;
-});
-
-// restaura se houver dados salvos
-const salvo = localStorage.getItem(STORAGE_KEY);
-if (salvo) {
-  document.querySelector(".planilha-container").innerHTML = salvo;
-}
-
-// salva ao editar
-document.addEventListener("input", () => {
-  const html = document.querySelector(".planilha-container").innerHTML;
-  localStorage.setItem(STORAGE_KEY, html);
-});
 
